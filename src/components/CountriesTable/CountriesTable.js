6d611ff4ca498acfe0af224d8b635d6208db2128
moorels <sales@micropls.com>
2021-05-28 08:45:31 +1000

    first

--- src/components/CountriesTable/CountriesTable.js
+++ src/components/CountriesTable/CountriesTable.js
@@ -1,32 +1,78 @@
 import styles from "./CountriesTable.module.css";
+import {useState} from "react";
+import Link from 'next/link';
+
+
+const orderBy = (countries, value,  direction) => {
+    if (direction === "asc") {
+    return [...countries].sort((a, b) => (a[value] > b[value] ? 1 : -1));
+}
+
+if (direction === "desc") {
+
+    return [...countries].sort((a, b) => (a[value] > b[value] ? -1 : 1));
+}
+
+return countries;
+
+};
+
 
 const CountriesTable = ({countries}) => {
+
+const [direction, setDirection] = useState();
+const [value, setValue] = useState();
+
+
+    const orderedCountries = orderBy(countries,value, direction) ;
+    const switchDirection = () => {
+    if (!direction) {
+        setDirection("desc");
+    } else if (direction === "desc") {
+        setDirection ("asc");
+
+    } else {
+        setDirection (null);
+    }
+
+    }
+
+    const setValueDirection  = (value) => {
+        switchDirection();
+        setValue(value);
+    }
+
     return (
-    
     <div>
-    
     <div className={styles.row}>
 
-    <button className={styles.heading_name}    >
+    <button className={styles.heading_population} 
+    onClick={() => setValueDirection("name")}    >
     <div>Name</div>
 </button>
-<button className ={styles.heading_name}    >
+
+<button className={styles.heading_population} 
+onClick={() => setValueDirection("population")}    >
     <div>Population</div>
+
+
 </button>
 
     </div>
     
-    
-
-{countries.map((country) => (
+{orderedCountries.map((country) => (
 
 
+<Link href = {`/country/${country.alpha3Code}`}>
 <div className={styles.row}>
 <div className={styles.name}>{country.name}</div>
 <div className={styles.population}>{country.population}</div>
 
 
 </div>
+
+</Link>
+
 ))}
 </div> 
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,20 +4,40 @@ import SearchInput from "../components/SearchInput/SearchInput";
 import Layout from '../components/Layout/Layout'
 import styles from '../styles/Home.module.css'
 import CountriesTable from '../components/CountriesTable/CountriesTable';
-
+import React, { useState } from "react";
 
 export default function Home({countries}) {
-  console.log();
-  return <Layout>
+  console.log(countries);
+
+const [keyword, setKeyword] = useState("")  ;
+
+
+const filteredCountries = countries.filter((country) => 
+country.name.toLowerCase().includes(keyword) ||
+country.region.toLowerCase().includes(keyword) ||
+country.subregion.toLowerCase().includes(keyword)
+);
+
+
+const onInputChange = (e) => {
+  e.preventDefault();
+
+  setKeyword(e.target.value.toLowerCase());
+};
+
+
+  return (
+  <Layout>
   <div className = {styles.counts}>Found {countries.length} countries</div>
  
 
-  <SearchInput placeholder ="Filterby Name, region or SubRegion"  />
+  <SearchInput placeholder ="Filterby Name, region or SubRegion" 
+  onChange={onInputChange} />
 
-  <CountriesTable countries ={countries}></CountriesTable>
+  <CountriesTable countries ={filteredCountries}/>
   </Layout>
 
-
+  );
 }
 
 
